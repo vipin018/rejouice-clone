095e778d616ec83df426a30a8960500222ea9816
vipin018 <vipinpathak018@gmail.com>
2025-01-09 00:00:10 +0530

    updated

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 // this is for the cursor to follow the mouse movement on the div 
+function cursorAnimation() {
 
-let page1Content = document.querySelector("#page1-content");
+    let page1Content = document.querySelector("#page1-content");
 
-let cursor = document.querySelector("#cursor");
+    let cursor = document.querySelector("#cursor");
 
-page1Content.addEventListener('mousemove', (dets) => {
-    // cursor.style.left = dets.clientX + 'px';
-    // cursor.style.top = dets.clientY + 'px';
+    page1Content.addEventListener('mousemove', (dets) => {
+        // cursor.style.left = dets.clientX + 'px';
+        // cursor.style.top = dets.clientY + 'px';
 
-    gsap.to(cursor, {
-        x: dets.clientX,
-        y: dets.clientY,
-        duration: 0.1
+        gsap.to(cursor, {
+            x: dets.clientX,
+            y: dets.clientY,
+            duration: 0.1
+        });
     });
-});
 
-page1Content.addEventListener('mouseenter', () => {
-    gsap.to(cursor, {
-        scale: 1,
+    page1Content.addEventListener('mouseenter', () => {
+        gsap.to(cursor, {
+            scale: 1,
+            opacity: 1,
+        });
     });
-});
 
-page1Content.addEventListener('mouseleave', () => {
-    gsap.to(cursor, {
-        scale: 0,
+    page1Content.addEventListener('mouseleave', () => {
+        gsap.to(cursor, {
+            scale: 0,
+            opacity: 0,
+        });
     });
-});
-
-
-
+}
+cursorAnimation();
